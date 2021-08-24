ec864a5384bb16293f064cf5aeb7b93c009ef697
Heighties <heighties@gmail.com>
2021-08-24 15:04:10 +0200

    Notions pour créer un chronomètre

---
+++ Main.java
@@ -0,0 +1,13 @@
+package fr.heighties.chrono;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Chrono chrono = new Chrono();
+		// Chrono chrono2 = new Chrono();
+		// Chrono chrono3 = new Chrono();
+
+	}
+
+}
